46913a5390a208ead53521875ed489bcd547a4cb
Thielison <67246254+thielison@users.noreply.github.com>
2024-05-06 23:19:10 -0300

    Disable webpack performance hints

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  performance: {
+    hints: false,
+  },
   mode: "production",
   entry: "./src/validations.js",
   devtool: "inline-source-map",
